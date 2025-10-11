77a9f417c753198cec2d57ef37f825a482fdbef9
Rodrigo Marques <rodrigo1marquesalves@gmail.com>
2025-10-11 06:24:41 +0000

    Reduza mais

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="w-full py-4 px-4 sm:px-6 lg:px-8 z-10 bg-black">
+    <header className="w-full py-2 px-4 sm:px-6 lg:px-8 z-10 bg-black">
       <div className="container mx-auto">
         <Link href="/" className="text-3xl md:text-4xl font-headline text-primary tracking-widest">
           LinkFlix
