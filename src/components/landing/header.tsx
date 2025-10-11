31acf09925d0e2c3d89a61878ef053958b47d91c
Rodrigo Marques <rodrigo1marquesalves@gmail.com>
2025-10-11 06:22:10 +0000

    LinkFlix (_for element div_)

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="absolute top-0 left-0 w-full py-6 px-4 sm:px-6 lg:px-8 z-10">
       <div className="container mx-auto">
         <Link href="/" className="text-3xl md:text-4xl font-headline text-primary tracking-widest">
-          BIOFLIX
+          LinkFlix
         </Link>
       </div>
     </header>
