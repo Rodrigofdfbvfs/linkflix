60e929cd666d0ce334c0ae031c5c3e9f8a244a46
Rodrigo Marques <rodrigo1marquesalves@gmail.com>
2025-10-20 12:25:01 +0000

    Centralize (_for element <LinkComponent>_)

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <header className="w-full py-5 px-4 sm:px-6 lg:px-8 z-10 bg-black">
-      <div className="container mx-auto">
+      <div className="container mx-auto text-center">
         <Link href="/" className="text-3xl md:text-4xl font-headline text-primary tracking-widest">
           LinkFlix
         </Link>
