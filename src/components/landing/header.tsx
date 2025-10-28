1f7f1fb5ee46b51c870ccbc2bd15a8338348f97b
Rodrigo Marques <rodrigo1marquesalves@gmail.com>
2025-10-28 02:59:11 +0000

    Diminua o gap o espaçamento vertical do nome linkflix para a imagem  (_f

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="w-full py-5 px-4 sm:px-6 lg:px-8 z-10 bg-black">
+    <header className="w-full py-4 px-4 sm:px-6 lg:px-8 z-10 bg-black">
       <div className="container mx-auto text-center">
         <Link href="/" className="text-4xl md:text-5xl font-headline text-primary">
           LinkFlix
