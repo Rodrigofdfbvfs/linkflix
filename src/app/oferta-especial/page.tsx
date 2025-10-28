d35a3a3724ae3a5748241508570d2a728eb38494
Rodrigo Marques <rodrigo1marquesalves@gmail.com>
2025-10-28 03:09:59 +0000

    https://pay.lowify.com.br/checkout?product_id=KNCUhu Coloque apenas ness

--- src/app/oferta-especial/page.tsx
+++ src/app/oferta-especial/page.tsx
@@ -84,7 +84,7 @@ export default function SpecialOfferPage() {
                                     <Button size="lg" className="w-full font-bold transition-all duration-300 hover:shadow-red-glow" onClick={() => handleRedirect('https://pay.lowify.com.br/checkout?product_id=5BFiL9')}>
                                         Quero o Premium com Desconto!
                                     </Button>
-                                    <Button variant="outline" className="w-full text-white/70 hover:text-white hover:bg-primary/10 border-primary" onClick={() => handleRedirect('https://pay.cakto.com.br/5mw5agw_603744')}>
+                                    <Button variant="outline" className="w-full text-white/70 hover:text-white hover:bg-primary/10 border-primary" onClick={() => handleRedirect('https://pay.lowify.com.br/checkout?product_id=KNCUhu')}>
                                         Não, obrigado. Quero apenas o plano básico.
                                     </Button>
                                 </CardFooter>
