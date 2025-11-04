5423cf30e607ca0b9b1012c9ab6c066bf14cd74a
Rodrigo Marques <rodrigo1marquesalves@gmail.com>
2025-11-04 22:45:09 +0000

    Deixe essa faixa um pouco mais grosso (_for element <DateBanner>_)

--- src/components/landing/date-banner.tsx
+++ src/components/landing/date-banner.tsx
@@ -24,7 +24,7 @@ export default function DateBanner() {
                 fontWeight: 'bold',
                 fontFamily: 'Arial',
                 backgroundColor: '#E50914',
-                padding: '12px 4px',
+                padding: '16px 4px',
                 textAlign: 'center',
             }}
         >
